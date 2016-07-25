a548649e8fae4a5ec1d7cc7ee05b626f70219819
Gerardo Báez <gerardojbaez@gmail.com>
2016-07-25 13:09:23 -0400

    Add function doc block

--- src/helpers.php
+++ src/helpers.php
@@ -2,6 +2,13 @@
 
 if ( ! function_exists('moneyFormat'))
 {
+	/**
+	 * Format number to a particular currency.
+	 *
+	 * @param float Amount to format
+	 * @param string Currency
+	 * @return string
+	 */
 	function moneyFormat($amount, $currency)
 	{
 		$money = new Gerardojbaez\Money\Money($amount, $currency);
