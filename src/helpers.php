e7164774dcfd29fea569cb8f3d90efedffb0be81
Gerardo Báez <gerardojbaez@gmail.com>
2016-07-25 12:20:29 -0400

    Fix helper function having public

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,7 @@
 
 if ( ! function_exists('moneyFormat'))
 {
-	public function moneyFormat($amount, $currency)
+	function moneyFormat($amount, $currency)
 	{
 		$money = Gerardojbaez\Money\Money($amount, $currency);
 
